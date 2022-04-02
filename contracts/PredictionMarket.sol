86cc57f9a55aba251b44bc0237221a9a1fabf5e7
avi <bina@Binas-MacBook-Pro.local>
2022-04-01 21:42:41 -0700

    created test cases for smart contract

--- contracts/PredictionMarket.sol
+++ contracts/PredictionMarket.sol
@@ -1,41 +1,41 @@
 pragma solidity ^0.8.10;
 
 contract PredictionMarket {
-    enum President { Biden, Trump }
+    enum Side { Biden, Trump }
     struct Result {
-        President winner;
-        President loser;
+        Side winner;
+        Side loser;
     }
     Result public result;
     bool public electionFinished;
 
-    mapping(President => uint) public bets;
-    mapping(address => mapping(President => uint)) public betsPerGambler;
+    mapping(Side => uint) public bets;
+    mapping(address => mapping(Side => uint)) public betsPerGambler;
     address public oracle; 
 
     constructor(address _oracle) {
         oracle = _oracle;
     }
 
-    function placeBet(President _president) external payable {
+    function placeBet(Side _side) external payable {
         require(electionFinished == false, 'Event has already passed');
-        bets[_president] += msg.value;
-        betsPerGambler[msg.sender][_president] += msg.value;
+        bets[_side] += msg.value;
+        betsPerGambler[msg.sender][_side] += msg.value;
     }
 
     function withdrawGain() external {
-        uint userBet = betsPerGambler[msg.sender][result.winner];
-        require(userBet > 0, 'You do not have a winning bet');
+        uint gamblerBet = betsPerGambler[msg.sender][result.winner];
+        require(gamblerBet > 0, 'You do not have a winning bet');
         require(electionFinished == true, 'The event has not finished yet');
         // Winner's gains are composed of original bet + proportional share of losing bet pool
-        uint gain = userBet + bets[result.loser] * userBet / bets[result.winner];
+        uint gain = gamblerBet + bets[result.loser] * gamblerBet / bets[result.winner];
         // Reset betters bets so user cannot withdraw multiple times
-        betsPerGambler[msg.sender][President.Biden] = 0;
-        betsPerGambler[msg.sender][President.Trump] = 0;
+        betsPerGambler[msg.sender][Side.Biden] = 0;
+        betsPerGambler[msg.sender][Side.Trump] = 0;
         payable(msg.sender).transfer(gain);
     }
 
-    function reportResult(President _winner, President _loser) external {
+    function reportResult(Side _winner, Side _loser) external {
         require(oracle == msg.sender, 'Only oracle is allowed');
         require(electionFinished == false, 'Event is finished');
         result.winner = _winner;
